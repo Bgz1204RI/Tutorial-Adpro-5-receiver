e0eb8cf5a36ac7da8e2572a0f0e604a3db2b1c36
Bgz1204RI <bagaszharif24@gmail.com>
2026-03-31 18:42:07 +0700

    Implement add function in Subscriber repository.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -14,3 +14,10 @@ pub struct NotificationRepository;
 impl NotificationRepository {
 
 }
+
+pub fn add(notification: Notification) -> Notification {
+    NOTIFICATIONS.write().unwrap()
+        .push(notification.clone());
+    return notification;
+}
+
